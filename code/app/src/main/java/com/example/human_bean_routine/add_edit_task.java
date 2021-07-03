6c2270f91ceaf72bf9fb5f3ef27f360acac43bb3
Bani Singh <bani.singh@uwaterloo.ca>
2021-07-03 18:10:32 -0400

    add task dashboard and add/edit task activites

---
+++ code/app/src/main/java/com/example/human_bean_routine/add_edit_task.java
@@ -0,0 +1,14 @@
+package com.example.human_bean_routine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class add_edit_task extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_edit_task);
+    }
+}
