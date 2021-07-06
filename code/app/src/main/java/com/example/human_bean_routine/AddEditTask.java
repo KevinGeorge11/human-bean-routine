7888797f46726b29ac2bc47b18ac129fd7e6ac62
Bani Singh <bani.singh@uwaterloo.ca>
2021-07-06 03:54:11 -0400

    fixed the method to returnToDashboard

--- code/app/src/main/java/com/example/human_bean_routine/AddEditTask.java
+++ code/app/src/main/java/com/example/human_bean_routine/AddEditTask.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddEditTask extends AppCompatActivity {
 
@@ -13,7 +14,7 @@ public class AddEditTask extends AppCompatActivity {
         setContentView(R.layout.activity_add_edit_task);
     }
 
-    public void returnToDashboard () {
+    public void returnToDashboard (View view) {
         Intent intent = new Intent(AddEditTask.this, TaskDashboard.class);
         startActivity(intent);
     }
