538e3775cc6ea8110b399164490ab80b7f50a4e7
Bani Singh <bani.singh@uwaterloo.ca>
2021-07-06 05:36:14 -0400

    added dummy array to populate categories

--- code/app/src/main/java/com/example/human_bean_routine/AddEditTask.java
+++ code/app/src/main/java/com/example/human_bean_routine/AddEditTask.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AddEditTask extends AppCompatActivity {
 
@@ -12,6 +14,13 @@ public class AddEditTask extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_edit_task);
+
+        // Filling out category spinner. TODO: Change to dynamic array
+        Spinner category_spinner = (Spinner) findViewById(R.id.spinnerCategory);
+        ArrayAdapter<CharSequence> category_adapter =
+                ArrayAdapter.createFromResource(this, R.array.categories_array,
+                        android.R.layout.simple_spinner_item);
+        category_spinner.setAdapter(category_adapter);
     }
 
     public void returnToDashboard (View view) {
