53b973c65859ce31c6cd1536b03b3e1a91e36982
Bani Singh <bani.singh@uwaterloo.ca>
2021-07-06 03:32:20 -0400

    added return to dashboard on click

--- code/app/src/main/java/com/example/human_bean_routine/add_edit_task.java
+++ code/app/src/main/java/com/example/human_bean_routine/add_edit_task.java
@@ -2,6 +2,7 @@ package com.example.human_bean_routine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class add_edit_task extends AppCompatActivity {
@@ -10,5 +11,10 @@ public class add_edit_task extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_edit_task);
+    }
+
+    public void returnToDashboard () {
+        Intent intent = new Intent(add_edit_task.this, task_dashboard.class);
+        startActivity(intent);
     }
 }
