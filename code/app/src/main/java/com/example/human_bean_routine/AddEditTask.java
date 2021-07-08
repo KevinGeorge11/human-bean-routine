9e4c1676fdd0a4ef371a9036c345915b27409f74
Bani Singh <bani.singh@uwaterloo.ca>
2021-07-07 21:49:27 -0400

    add save button on click

--- code/app/src/main/java/com/example/human_bean_routine/AddEditTask.java
+++ code/app/src/main/java/com/example/human_bean_routine/AddEditTask.java
@@ -3,13 +3,18 @@ package com.example.human_bean_routine;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import java.util.Calendar;
 import java.util.TimeZone;
@@ -37,38 +42,78 @@ public class AddEditTask extends AppCompatActivity {
         repetition_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         repetition_spinner.setAdapter(repetition_adapter);
 
+
+
         // Setting tvStartDate on click listener
-        TextView tvStartDate = (TextView) findViewById(R.id.tvStartDate);
-        tvStartDate.setOnClickListener(new View.OnClickListener() {
+        EditText editEndDate = (EditText) findViewById(R.id.editEndDate);
+        final Calendar endDateCal = Calendar.getInstance();
+        DatePickerDialog.OnDateSetListener etEndDateListener =
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        endDateCal.set(Calendar.YEAR, year);
+                        endDateCal.set(Calendar.MONTH, month);
+                        endDateCal.set(Calendar.DAY_OF_MONTH, day);
+                        editEndDate.setText( String.valueOf(day) + "/" +  String.valueOf(month)
+                                + "/" +  String.valueOf(year));
+                    }
+                };
+        editEndDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DatePickerDialog picker = openDatePicker(v);
+
+                DatePickerDialog picker = new DatePickerDialog( AddEditTask.this, etEndDateListener,
+                        endDateCal.get(Calendar.YEAR), endDateCal.get(Calendar.MONTH),
+                        endDateCal.get(Calendar.DAY_OF_MONTH));
                 picker.show();
             }
             });
 
+
+        // Setting etStartDate on click listener
+        EditText etStartDate = (EditText) findViewById(R.id.etStartDate);
+        final Calendar startDateCal = Calendar.getInstance();
+        DatePickerDialog.OnDateSetListener etStartDateListener =
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        myOnDateSet(etStartDate, view, year, month, day);
+                    }
+                };
+        etStartDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog picker = openDatePicker(v, etStartDateListener, startDateCal);
+                picker.show();
+            }
+        });
+
+        // On clicking save button
+        Button saveButton = findViewById(R.id.btnSave);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),TaskDashboard.class);
+                startActivity(i);
+            }
+        });
+
     }
 
-    public void returnToDashboard (View view) {
-        Intent intent = new Intent(AddEditTask.this, TaskDashboard.class);
-        startActivity(intent);
+
+    // Sets date to the Edittext tv
+    public void myOnDateSet(EditText et, DatePicker view, int selectedYear, int selectedMonth, int selectedDay) {
+        String year = String.valueOf(selectedYear);
+        String month = String.valueOf(selectedMonth + 1);
+        String day = String.valueOf(selectedDay);
+        et.setText(day + "/" + month + "/" + year);
     }
 
 
-    private DatePickerDialog.OnDateSetListener datePickerListener = new DatePickerDialog.OnDateSetListener() {
-
-        // when dialog box is closed, below method will be called.
-        public void onDateSet(DatePicker view, int selectedYear, int selectedMonth, int selectedDay) {
-            String year = String.valueOf(selectedYear);
-            String month = String.valueOf(selectedMonth + 1);
-            String day = String.valueOf(selectedDay);
-            TextView tvDt = (TextView) findViewById(R.id.tvStartDate);
-            tvDt.setText(day + "/" + month + "/" + year);
-        }
-    };
-
-    public DatePickerDialog openDatePicker (View view) {
-        Calendar cal = Calendar.getInstance(TimeZone.getDefault());
+    // Returns a date picker
+    public DatePickerDialog openDatePicker (View view, DatePickerDialog.OnDateSetListener datePickerListener,
+                                            Calendar cal) {
         DatePickerDialog datePicker = new DatePickerDialog(this,
                 R.style.Theme_Humanbeanroutine,
                 datePickerListener,
@@ -76,8 +121,8 @@ public class AddEditTask extends AppCompatActivity {
                 cal.get(Calendar.MONTH),
                 cal.get(Calendar.DAY_OF_MONTH));
 
-        datePicker.setCancelable(false);
-        datePicker.setTitle("Select the date");
+      //  datePicker.setCancelable(false);
+       // datePicker.setTitle("Select the date");
 
         return datePicker;
     }
