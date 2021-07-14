319212d96d0a07ebfd96db1af9ca6461e68253bf
gloriajson <gloria.son@uwaterloo.ca>
2021-07-14 15:28:47 -0400

    Added Previous Puzzle screen

---
+++ code/app/src/main/java/com/example/human_bean_routine/PreviousPuzzlesActivity.java
@@ -0,0 +1,14 @@
+package com.example.human_bean_routine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PreviousPuzzlesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_previous_puzzles);
+    }
+}
