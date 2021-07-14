212d5b69bb95986ad8d64171c5e9c58d70aea8a8
Bani Singh <bani.singh@uwaterloo.ca>
2021-07-14 15:08:11 -0400

    added fields for Task Class

--- code/app/src/main/java/Task.java
+++ code/app/src/main/java/Task.java
@@ -1,2 +1,14 @@
 public class Task {
+    private Integer id;
+    private String name;
+    private Boolean complete;
+    private String description;
+    private String category;
+    private String startDate;
+    private String startTime;
+    private String endDate;
+    private String endTime;
+    private String repeat;
+    private String reminderDate;
+    private String reminderTime;
 }
