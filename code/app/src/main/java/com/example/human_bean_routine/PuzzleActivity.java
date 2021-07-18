32338dc3afc1d3ddf33f8900f1c768ecce037b7a
g3son <gloriason@Glorias-MacBook-Pro.local>
2021-07-18 11:32:27 -0400

    Added strings to string.xml and added base for old puzzle piece modal navigation

--- code/app/src/main/java/com/example/human_bean_routine/PuzzleActivity.java
+++ code/app/src/main/java/com/example/human_bean_routine/PuzzleActivity.java
@@ -7,15 +7,24 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 import java.util.ArrayList;
 
 public class PuzzleActivity extends AppCompatActivity {
 
     ArrayList<Bitmap> pieces;
+    PopupWindow modal;
+    int numPieces = 12;
+    int rows = 4;
+    int cols = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,22 +47,15 @@ public class PuzzleActivity extends AppCompatActivity {
         });
 
         Button previousPuzzles = findViewById(R.id.bPreviousPuzzles);
-        previousPuzzles.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), PreviousPuzzlesActivity.class);
-                startActivity(i);
-            }
+        previousPuzzles.setOnClickListener(v -> {
+            Intent i = new Intent(getApplicationContext(), PreviousPuzzlesActivity.class);
+            startActivity(i);
         });
     }
 
     private ArrayList<Bitmap> splitImage() {
-        int piecesNumber = 12;
-        int rows = 4;
-        int cols = 3;
-
         ImageView imageView = findViewById(R.id.ivPuzzle);
-        ArrayList<Bitmap> pieces = new ArrayList<>(piecesNumber);
+        ArrayList<Bitmap> pieces = new ArrayList<>(numPieces);
 
         // Get the bitmap of the source image
         BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
@@ -64,16 +66,39 @@ public class PuzzleActivity extends AppCompatActivity {
         int pieceHeight = bitmap.getHeight()/rows;
 
         // Create each bitmap piece and add it to the resulting array
-        int yCoord = 0;
+        int y = 0;
         for (int row = 0; row < rows; row++) {
-            int xCoord = 0;
+            int x = 0;
             for (int col = 0; col < cols; col++) {
-                pieces.add(Bitmap.createBitmap(bitmap, xCoord, yCoord, pieceWidth, pieceHeight));
-                xCoord += pieceWidth;
+//                int x = pieceWidth*row;
+//                int y = pieceHeight*col;
+                pieces.add(Bitmap.createBitmap(bitmap, x, y, pieceWidth, pieceHeight));
+
+                x += pieceWidth;
             }
-            yCoord += pieceHeight;
+            y += pieceHeight;
         }
 
         return pieces;
+    }
+
+    public void showPreviousPuzzleModal() {
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.old_puzzle_piece_modal, null);
+
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        modal = new PopupWindow(popupView, width, height, focusable);
+        modal.showAtLocation(findViewById(R.id.taskList), Gravity.CENTER, 0, 0);
+
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                modal.dismiss();
+                return true;
+            }
+        });
     }
 }
