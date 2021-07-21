85ce05c19168000b6c75e6dc03b71b9176be371d
Bani Singh <bani.singh@uwaterloo.ca>
2021-07-21 19:58:51 -0400

    added method 'navigate'

--- code/app/src/main/java/com/example/human_bean_routine/MainActivity.java
+++ code/app/src/main/java/com/example/human_bean_routine/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.human_bean_routine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /* navigates to the [activity] on clicking [menuItem] */
+    private void navigate(Activity activity, MenuItem menuItem) {
+        // TODO: make menuItem highlighted (change color)
+        Intent intent = new Intent(this, activity.getClass());
+        startActivity(intent);
     }
 }
