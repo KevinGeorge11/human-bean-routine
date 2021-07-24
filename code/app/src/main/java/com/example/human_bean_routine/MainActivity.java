368d4a1ffc71936fc4a477f27e4a62c9a235f1ed
Bani Singh <bani.singh@uwaterloo.ca>
2021-07-24 19:04:05 -0400

    added some code for navigation

--- code/app/src/main/java/com/example/human_bean_routine/MainActivity.java
+++ code/app/src/main/java/com/example/human_bean_routine/MainActivity.java
@@ -3,8 +3,8 @@ package com.example.human_bean_routine;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import android.view.MenuItem;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,38 +13,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Bottom navigation buttons
-        BottomNavigationItemView miTasks = findViewById(R.id.miTasks);
-        BottomNavigationItemView miPuzzle = findViewById(R.id.miPuzzle);
-        BottomNavigationItemView miCategories = findViewById(R.id.miCategories);
-
-        // Bottom navigation on click listeners
-        miTasks.setOnClickListener(new View.OnClickListener() {
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bnvMenu);
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                navigate(new Intent(MainActivity.this, TaskDashboard.class), miTasks);
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.miPuzzle:
+                        Intent puzzleIntent = new Intent(MainActivity.this, PuzzleActivity.class);
+                        startActivity(puzzleIntent);
+                        break;
+                    case R.id.miTasks:
+                        Intent taskIntent = new Intent(MainActivity.this, TaskDashboard.class);
+                        startActivity(taskIntent);
+                        break;
+                    // TODO: add case for categories and settings
+                }
+                return false;
             }
         });
-
-/*
-        miCategories.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                navigate(new Intent(MainActivity.this, CategoryActivity.class), miCategories);
-            }
-        });*/
-
-        miPuzzle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                navigate(new Intent(MainActivity.this, PuzzleActivity.class), miPuzzle);
-            }
-        });
-    }
-
-    /* starts activity [intent] on clicking [menuItem] */
-    private void navigate(Intent intent, BottomNavigationItemView menuItem) {
-        // TODO: make menuItem highlighted (change color)
-        startActivity(intent);
     }
 }
