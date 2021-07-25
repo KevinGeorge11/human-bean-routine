cd9c1bcec7f54db979c2ab49b846161bc33e3e60
Bani Singh <bani.singh@uwaterloo.ca>
2021-07-25 17:58:05 -0400

    added bottom nav to settings

--- code/app/src/main/java/com/example/human_bean_routine/SettingsActivity.java
+++ code/app/src/main/java/com/example/human_bean_routine/SettingsActivity.java
@@ -1,14 +1,17 @@
 package com.example.human_bean_routine;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.human_bean_routine.databinding.ActivitySettingsBinding;
@@ -19,6 +22,25 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bnvMenu);
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.miPuzzle:
+                        Intent puzzleIntent = new Intent(SettingsActivity.this, PuzzleActivity.class);
+                        startActivity(puzzleIntent);
+                        break;
+                    case R.id.miTasks:
+                        Intent taskIntent = new Intent(SettingsActivity.this, TaskDashboard.class);
+                        startActivity(taskIntent);
+                        break;
+                    // TODO: add case for categories
+                }
+                return false;
+            }
+        });
 
     }
 }
