0df0576e8dae08fd9c690b662a48094939559136
Bani Singh <bani.singh@uwaterloo.ca>
2021-07-25 17:58:18 -0400

    deleted bottom nav from main activity

--- code/app/src/main/java/com/example/human_bean_routine/MainActivity.java
+++ code/app/src/main/java/com/example/human_bean_routine/MainActivity.java
@@ -33,24 +33,5 @@ public class MainActivity extends AppCompatActivity {
             // Intent intent = new Intent((MainActivity.this, CategoriesSetupActivity));
             // startActivity(intent);
         }
-
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bnvMenu);
-        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.miPuzzle:
-                        Intent puzzleIntent = new Intent(MainActivity.this, PuzzleActivity.class);
-                        startActivity(puzzleIntent);
-                        break;
-                    case R.id.miTasks:
-                        Intent taskIntent = new Intent(MainActivity.this, TaskDashboard.class);
-                        startActivity(taskIntent);
-                        break;
-                    // TODO: add case for categories and settings
-                }
-                return false;
-            }
-        });
     }
 }
