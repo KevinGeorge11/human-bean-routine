364506edfdcdfe32abf6d352f2f588a3b9f554be
Bani Singh <bani.singh@uwaterloo.ca>
2021-07-25 17:54:27 -0400

    added settings scrolling activity

---
+++ code/app/src/main/java/com/example/human_bean_routine/SettingsActivity.java
@@ -0,0 +1,28 @@
+package com.example.human_bean_routine;
+
+import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import com.example.human_bean_routine.databinding.ActivitySettingsBinding;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    private ActivitySettingsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivitySettingsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+}
