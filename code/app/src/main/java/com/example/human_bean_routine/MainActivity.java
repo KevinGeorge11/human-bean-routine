e21dc970fa8d49a129652da86ac116c88ac75c24
Bani Singh <bani.singh@uwaterloo.ca>
2021-07-25 16:09:51 -0400

    added skeleton code for startup
    redirecting to right screen on startup, setting up preferences

--- code/app/src/main/java/com/example/human_bean_routine/MainActivity.java
+++ code/app/src/main/java/com/example/human_bean_routine/MainActivity.java
@@ -1,17 +1,37 @@
 package com.example.human_bean_routine;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String hbrPrefs = "hbrPreferences";
+    public static final String launched = "launched";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Launch and preferences management
+        SharedPreferences sharedPreferences = getSharedPreferences(hbrPrefs, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        if (sharedPreferences.contains(launched)) {
+            Intent intent = new Intent(MainActivity.this, TaskDashboard.class);
+            startActivity(intent);
+        } else {
+            // TODO: add method to populate preferences
+            editor.putBoolean(launched, true);
+            editor.apply();
+            // Intent intent = new Intent((MainActivity.this, CategoriesSetupActivity));
+            // startActivity(intent);
+        }
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bnvMenu);
         navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
