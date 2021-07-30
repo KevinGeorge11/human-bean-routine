d785f9af4b3f44c2ba3581863f2d94f1a4d2d9da
Bani Singh <bani.singh@uwaterloo.ca>
2021-07-30 17:22:30 -0400

    added startup screen

--- code/app/src/main/java/com/example/human_bean_routine/MainActivity.java
+++ code/app/src/main/java/com/example/human_bean_routine/MainActivity.java
@@ -6,7 +6,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,15 +28,28 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences sharedPreferences = getSharedPreferences(hbrPrefs, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         if (sharedPreferences.contains(launched)) {
-            Intent intent = new Intent(MainActivity.this, TaskDashboard.class);
-            startActivity(intent);
+            final Handler screenTimerHandler = new Handler(Looper.getMainLooper());
+            screenTimerHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(MainActivity.this, TaskDashboard.class);
+                    startActivity(intent);
+                }
+            }, 2000);
         } else {
             // TODO: add method to populate preferences
             editor.putBoolean(launched, true);
             editor.apply();
-            // TODO: show a startup screen
-            // Intent intent = new Intent((MainActivity.this, CategoriesSetupActivity));
-            // startActivity(intent);
+            Button btnStart = findViewById(R.id.btnStart);
+            btnStart.setVisibility(View.VISIBLE);
+            btnStart.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Intent intent = new Intent((MainActivity.this, CategoriesSetupActivity));
+                    // startActivity(intent);
+                }
+            });
+
         }
     }
 }
